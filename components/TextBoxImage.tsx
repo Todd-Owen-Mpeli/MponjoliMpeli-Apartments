fc94674b06578006d52fd710a9eb015a3d02f321
Todd Mpeli <toddowenmpeli@rocketmail.com>
2023-03-25 13:54:26 +0000

    Styling tweaks/Changes for ipad.

--- components/TextBoxImage.tsx
+++ components/TextBoxImage.tsx
@@ -23,17 +23,17 @@ const TextBoxImage: FC<IProps> = ({title, image, subtitle, paragraph}) => {
 		<section className="py-0 lg:py-24 bg-white lg:px-8">
 			<div className="lg:container px-0 mx-auto">
 				<div className="relative flex flex-col-reverse lg:flex-row">
-					<div className="w-full lg:w-4/5 lg:ml-auto">
+					<div className="w-full xl:w-4/5 xl:ml-auto bg-darkGreen lg:bg-white">
 						<motion.div variants={fadeIn}>
 							<Image
 								alt={image?.altText}
 								src={image?.sourceUrl}
 								width={image?.mediaDetails?.width}
 								height={image?.mediaDetails?.height}
-								className="relative object-cover mx-auto mt-0 rounded-none lg:rounded-lg max-h-[450px] lg:max-h-full lg:mt-5 max-w-full lg:max-w-xl xl:max-w-4xl"
+								className="relative object-cover object-center mx-auto mt-0 rounded-none lg:rounded-lg max-h-[650px] xl:max-h-full lg:mt-5 max-w-full lg:max-w-3xl xl:max-w-4xl"
 							/>
 						</motion.div>
-						<div className="top-0 left-0 max-w-3xl px-6 py-20 mx-auto text-center rounded-none lg:rounded-lg bg-darkGreen lg:absolute xl:mx-0 lg:px-12 lg:py-20">
+						<div className="top-0 left-0 max-w-3xl px-6 py-20 mx-auto text-center rounded-none lg:rounded-lg bg-darkGreen lg:absolute lg:mx-0 lg:px-12 lg:py-20">
 							<span className="font-medium tracking-widest text-goldPrime text-medium">
 								{subtitle}
 							</span>
