ab8da466bb76087c2108cf95a9ee1cc65610409e
Todd Mpeli <toddowenmpeli@rocketmail.com>
2023-03-27 23:08:46 +0100

    Added content and components to the apartments page.

---
+++ pages/apartments.tsx
@@ -0,0 +1,229 @@
+// Import
+import {gql} from "@apollo/client";
+import {motion} from "framer-motion";
+import {client} from "../config/apollo";
+import {getThemesOptionsContent} from "../lib/themesOptions";
+import {
+	getMainMenuLinks,
+	getHeroMenuLinks,
+	getFooterMenuLinks,
+	getLocationMenuLinks,
+} from "../lib/MenuLinks";
+
+// Components
+import Footer from "@/components/Footer";
+import HeroThree from "@/components/HeroThree";
+import ImageGrid from "@/components/ImageGrid";
+import MetaTag from "@/components/Meta/MetaTag";
+import ContactBanner from "@/components/ContactBanner";
+
+const apartments = ({
+	seo,
+	content,
+	pageTitle,
+	mainMenuLinks,
+	heroMenuLinks,
+	footerMenuLinks,
+	locationMenuLinks,
+	themesOptionsContent,
+}: any) => {
+	return (
+		<motion.div
+			exit={{
+				opacity: 0,
+			}}
+			initial="initial"
+			animate="animate"
+		>
+			{/* <!--===== META TAG =====--> */}
+			<MetaTag title={pageTitle} seo={seo} />
+
+			<main>
+				<HeroThree
+					title={content?.heroSection?.title}
+					paragraph={content?.heroSection?.paragraph}
+					mainMenuLinks={mainMenuLinks?.mainMenuLinks}
+					heroMenuLinks={heroMenuLinks?.heroMenuLinks}
+					locationMenuLinks={locationMenuLinks?.locationMenuLinks}
+					twitterLink={themesOptionsContent?.themesOptions?.twitterLink}
+					mbeziContent={themesOptionsContent?.themesOptions?.mbeziContent}
+					linkedinLink={themesOptionsContent?.themesOptions?.linkedinLink}
+					facebookLink={themesOptionsContent?.themesOptions?.facebookLink}
+					instagramLink={themesOptionsContent?.themesOptions?.instagramLink}
+					mbweniContent={themesOptionsContent?.themesOptions?.mbweniContent}
+					backgroundImage={content?.heroSection?.backgroundImage?.sourceUrl}
+				/>
+
+				<ContactBanner
+					title={content?.contactBanner?.title}
+					paragraph={content?.contactBanner?.paragraph}
+					buttonLink={content?.contactBanner?.buttonLink}
+					backgroundImage={content?.contactBanner?.image?.sourceUrl}
+				/>
+
+				<ImageGrid
+					image={content?.imageGrid?.image}
+					imageTwo={content?.imageGrid?.imageTwo}
+					imageThree={content?.imageGrid?.imageThree}
+					imageFour={content?.imageGrid?.imageFour}
+					imageFive={content?.imageGrid?.imageFive}
+					imageSix={content?.imageGrid?.imageSix}
+				/>
+
+				<Footer
+					footerMenuLinks={footerMenuLinks?.footerMenuLinks}
+					twitterLink={themesOptionsContent?.themesOptions?.twitterLink}
+					linkedinLink={themesOptionsContent?.themesOptions?.linkedinLink}
+					facebookLink={themesOptionsContent?.themesOptions?.facebookLink}
+					instagramLink={themesOptionsContent?.themesOptions?.instagramLink}
+				/>
+			</main>
+		</motion.div>
+	);
+};
+
+export default apartments;
+
+export async function getStaticProps() {
+	const getApartmentsPageContent: any = gql`
+		{
+			pageTitle: pages(where: {id: 815, status: PUBLISH}) {
+				edges {
+					node {
+						title
+					}
+				}
+			}
+			mainContent: pages(where: {id: 815, status: PUBLISH}) {
+				edges {
+					node {
+						seo {
+							canonical
+							cornerstone
+							focuskw
+							fullHead
+							metaDesc
+							metaKeywords
+							metaRobotsNofollow
+							metaRobotsNoindex
+							opengraphAuthor
+							opengraphDescription
+							opengraphImage {
+								mediaItemUrl
+							}
+							opengraphModifiedTime
+							opengraphPublishedTime
+							opengraphPublisher
+							opengraphSiteName
+							opengraphTitle
+							opengraphType
+							opengraphUrl
+							readingTime
+							title
+							twitterDescription
+							twitterTitle
+							twitterImage {
+								mediaItemUrl
+							}
+						}
+						apartmentsPage {
+							heroSection {
+								title
+								paragraph
+								backgroundImage {
+									sourceUrl
+								}
+							}
+							contactBanner {
+								title
+								paragraph
+								image {
+									sourceUrl
+								}
+								buttonLink {
+									url
+									title
+									target
+								}
+							}
+							imageGrid {
+								image {
+									altText
+									sourceUrl
+									mediaDetails {
+										height
+										width
+									}
+								}
+								imageTwo {
+									altText
+									sourceUrl
+									mediaDetails {
+										height
+										width
+									}
+								}
+								imageThree {
+									altText
+									sourceUrl
+									mediaDetails {
+										height
+										width
+									}
+								}
+								imageFour {
+									altText
+									sourceUrl
+									mediaDetails {
+										height
+										width
+									}
+								}
+								imageFive {
+									altText
+									sourceUrl
+									mediaDetails {
+										height
+										width
+									}
+								}
+								imageSix {
+									altText
+									sourceUrl
+									mediaDetails {
+										height
+										width
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	`;
+
+	const response: any = await client.query({
+		query: getApartmentsPageContent,
+	});
+
+	const mainMenuLinks: object = await getMainMenuLinks();
+	const heroMenuLinks: object = await getHeroMenuLinks();
+	const footerMenuLinks: object = await getFooterMenuLinks();
+	const locationMenuLinks: object = await getLocationMenuLinks();
+	const themesOptionsContent: object = await getThemesOptionsContent();
+
+	return {
+		props: {
+			mainMenuLinks,
+			heroMenuLinks,
+			footerMenuLinks,
+			locationMenuLinks,
+			themesOptionsContent,
+			seo: response?.data?.mainContent?.edges[0]?.node?.seo,
+			pageTitle: response?.data?.pageTitle?.edges[0]?.node?.title,
+			content: response.data?.mainContent?.edges[0]?.node?.apartmentsPage,
+		},
+		revalidate: 60,
+	};
+}
