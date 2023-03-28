7a9a954c5442b43b9d4780406e9eba7a3f45e37e
Todd Mpeli <toddowenmpeli@rocketmail.com>
2023-03-28 23:22:18 +0100

    Styling tweaks

--- components/Cards/ApartmentCard.tsx
+++ components/Cards/ApartmentCard.tsx
@@ -15,12 +15,12 @@ const ApartmentCard: FC<IProps> = ({title, link, backgroundImage}) => {
 	return (
 		<Link href={link?.url} target={link?.target}>
 			<div
-				className="w-full h-full min-h-[450px] bg-center bg-no-repeat bg-cover p-8 "
+				className="group w-full h-full min-h-[450px] bg-center bg-no-repeat bg-cover p-8 "
 				style={{
 					backgroundImage: `url("${backgroundImage}")`,
 				}}
 			>
-				<div className="relative mx-auto h-full w-full min-h-[450px] border-white border-[4px]">
+				<div className="relative mx-auto h-full w-full min-h-[450px] border-goldPrime border-[4px] border-opacity-50 transition-all ease-in-out duration-500 group-hover:border-goldPrimeDark">
 					<div className="absolute inset-0"></div>
 					<div className="absolute inset-0 flex items-center justify-center">
 						<h2 className="text-xl font-[400] text-center text-white lg:text-2xl">
